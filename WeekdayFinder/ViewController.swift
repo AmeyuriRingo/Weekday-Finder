ddd896bb5ab1076633aca7e957802745ce7bd6a0
Nickita <Nickos_K@mail.ru>
2018-10-15 11:41:37 +0300

    s

---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Ringo_02 on 10/14/18.
+//  Copyright © 2018 Ringo_02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DateTF: UITextField!
+    @IBOutlet weak var MonthTF: UITextField!
+    @IBOutlet weak var YearTF: UITextField!
+
+    @IBOutlet weak var ResultLabel: UILabel!
+
+   
+    @IBAction func findWeekDay() {
+        
+        let caledndar = Calendar.current
+        
+        var dateComponents = DateComponents()
+        guard let day = DateTF.text, let month = DateTF.text, let year = DateTF.text else { return }
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+
+        guard let  date = caledndar.date(from: dateComponents) else { return }
+        
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "EEEE"
+        
+        let weekday = dateFormater.string(from: date)
+        
+        ResultLabel.text = weekday
+    }
+    
+    let caledndar = Calendar.current
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true )
+    }
+}
+
